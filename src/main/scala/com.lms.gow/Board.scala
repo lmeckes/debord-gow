19f7ebeb88cc4aca3b0de24dfbe970d16f450371
Louis Meckes <louis.meckes@gmail.com>
2015-12-14 17:49:08 +0100

    wip

--- src/main/scala/com.lms.gow/Board.scala
+++ src/main/scala/com.lms.gow/Board.scala
@@ -2,27 +2,17 @@ package main.scala.com.lms.gow
 
 case class Tile(char: Char, speed: Int, range: Int, attack: Int, defense: Int)
 
-object Void extends Tile('.', 0, 0, 0, 0)
-
-object Arsenal extends Tile('A', 0, 0, 0, 0)
-
-object Cannon extends Tile('C', 1, 3, 5, 8)
-
-object SwiftCannon extends Tile('N', 2, 3, 5, 8)
-
-object Relay extends Tile('R', 1, 0, 0, 1)
-
-object SwiftRelay extends Tile('E', 2, 0, 0, 1)
-
-object Infantry extends Tile('I', 1, 2, 4, 6)
-
-object Cavalry extends Tile('V', 2, 2, 4, 5)
-
-object Fortress extends Tile('F', 0, 0, 0, 4)
-
+object VoidTile     extends Tile('.', 0, 0, 0, 0)
+object Arsenal      extends Tile('A', 0, 0, 0, 0)
+object Cannon       extends Tile('C', 1, 3, 5, 8)
+object SwiftCannon  extends Tile('N', 2, 3, 5, 8)
+object Relay        extends Tile('R', 1, 0, 0, 1)
+object SwiftRelay   extends Tile('E', 2, 0, 0, 1)
+object Infantry     extends Tile('I', 1, 2, 4, 6)
+object Cavalry      extends Tile('V', 2, 2, 4, 5)
+object Fortress     extends Tile('F', 0, 0, 0, 4)
 object MountainPass extends Tile('=', 0, 0, 0, 2)
-
-object Mountain extends Tile('M', 0, 0, 0, 0)
+object Mountain     extends Tile('M', 0, 0, 0, 0)
 
 class Board {
 
@@ -36,39 +26,28 @@ class Board {
   val startingTerrain =
     scala.io.Source.fromFile("src/main/resources/init.board")
       .mkString
-      .toUpperCase
       .filter(_ > ' ')
-      .map(t =>
-        if (terrainTiles.map(_.char).contains(t))
-          t
-        else Void.char
-      )
+      .toUpperCase
+      .map((terrainTiles.map(_.char) zip terrainTiles).toMap.get(_).getOrElse(VoidTile))
 
   val startingUnits =
     scala.io.Source.fromFile("src/main/resources/init.units")
       .mkString
       .filter(_ > ' ')
-      .map(t =>
-        if (unitTiles.map(_.char).contains(t.toUpper))
-          t
-        else Void.char
-      )
+      .toUpperCase
+      .map((unitTiles.map(_.char) zip unitTiles).toMap.get(_).getOrElse(VoidTile))
 
   var currentUnits = startingUnits
 
-  def tileFromChar(char: Char): Tile = {
-    Void
-  }
-
   def getTile(x: Int, y: Int): (Tile, Tile) = {
     val coord = x + (y * terrainWidth)
-    (tileFromChar(startingTerrain(coord)), tileFromChar(startingUnits(coord)))
+    (startingTerrain(coord), startingUnits(coord))
   }
 
   def printCurrentBoard {
 
     for (tile <- currentUnits.zipWithIndex) {
-      val t = tile._1
+      val t = tile._1.char
       print(s"$t  ")
       if ((tile._2 + 1) % terrainWidth == 0)
         print("\n")
--- src/test/scala/TestBoard.scala
+++ src/test/scala/TestBoard.scala
@@ -1,4 +1,4 @@
-import main.scala.com.lms.gow.Board
+import main.scala.com.lms.gow.{VoidTile, Board}
 import org.scalatest._
 
 class TestBoard extends FlatSpec with Matchers {
@@ -10,7 +10,14 @@ class TestBoard extends FlatSpec with Matchers {
 
   "Initial Units" should "be well placed" in {
     val board = new Board()
-    board.printCurrentBoard
+    val topUnit = board.startingUnits.take((board.terrainWidth * board.terrainHeight) / 2)
+    val bottomUnit = board.startingUnits.reverse.take((board.terrainWidth * board.terrainHeight) / 2)
+
+    println(topUnit.map(_.char).filterNot(_.equals(VoidTile.char)))
+    println(bottomUnit.map(_.char).filterNot(_.equals(VoidTile.char)))
+
+    assert(topUnit.length == bottomUnit.length)
+
   }
 
 }
