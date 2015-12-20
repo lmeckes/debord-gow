5a2283eb32236c0474c1ca7b885d2a44dab1a50f
Louis Meckes <louis.meckes@gmail.com>
2015-12-20 21:22:13 +0100

    com. lines impl. improvement

--- src/main/scala/com.lms.gow/App.scala
+++ src/main/scala/com.lms.gow/App.scala
@@ -3,7 +3,7 @@ package com.lms.gow
 
 import javafx.application.Application
 import javafx.scene.Scene
-import javafx.scene.canvas.Canvas
+import javafx.scene.canvas.{Canvas, GraphicsContext}
 import javafx.scene.effect.PerspectiveTransform
 import javafx.scene.image.Image
 import javafx.scene.layout.Pane
@@ -13,6 +13,7 @@ import javafx.stage.Stage
 
 import com.lms.gow.model._
 
+import scala.collection._
 import scala.util.Random
 
 class App extends Application {
@@ -22,11 +23,33 @@ class App extends Application {
   val width = tileSize * Rules.terrainWidth
   val height = tileSize * Rules.terrainHeight
 
+  def start(stage: Stage) {
+
+    model.move(14, 10, 13, 10)
+
+    val board = createBoardPane
+    setPerspective(board)
+    val scene = new Scene(board, Color.WHITESMOKE)
+
+    stage setTitle ("Game of War")
+    stage setScene (scene)
+    stage show
+  }
+
   def createBoardPane = {
 
     val boardPane = new Pane
 
-    def createTileSquare(x: Int, y: Int) = {
+    def scanBoardCoordinates(func: (Int, Int) => Unit) {
+      (0 until Rules.terrainWidth).foreach(x => {
+        (0 until Rules.terrainHeight).foreach(y => {
+          func(x, y)
+        })
+      })
+    }
+
+    def drawTiles(x: Int, y: Int): Unit = {
+
       val tileSquare = new Rectangle(x * tileSize, y * tileSize, tileSize, tileSize);
       if ((x + y) % 2 == 0) {
         tileSquare.setFill(Color.WHITE)
@@ -34,79 +57,90 @@ class App extends Application {
         tileSquare.setFill(Color.WHITESMOKE)
       }
       boardPane.getChildren().add(tileSquare)
-    }
-
-    def createTile(x: Int, y: Int) = {
 
       val canvas = new Canvas(tileSize, tileSize)
-      val gc = canvas.getGraphicsContext2D()
+      val gc = canvas.getGraphicsContext2D
       if (Random.nextBoolean())
         canvas.setScaleX(-1)
       canvas.setLayoutX(x * tileSize)
       canvas.setLayoutY(y * tileSize)
       boardPane.getChildren().add(canvas)
 
-      def drawTile(t: Tile): Unit = {
-        if (!t.eq(VoidTile)) {
+      drawTile(model.getTerrainTile(x, y), gc)
+      drawTile(model.getUnitTile(x, y), gc)
 
-          val tileImage = new Image("tiles/" + t.char.toString + ".png")
-          gc.drawImage(tileImage, 0, 0, tileSize, tileSize)
+      val canvas2 = new Canvas(tileSize, tileSize)
+      val gc2 = canvas2.getGraphicsContext2D
+      canvas2.setLayoutX(x * tileSize)
+      canvas2.setLayoutY(y * tileSize)
+      boardPane.getChildren().add(canvas2)
 
-          // Draw color bar
-          if (t.isUnit) {
-            if (t.isPlayer1)
-              gc.setFill(Color.BLUE)
-            else
-              gc.setFill(Color.RED)
-            gc.fillRect(0, tileSize - tileSize / 15, tileSize, tileSize / 15)
-          }
-
-          // Draw lines of communication
-          if (t.isCom) {
-            val canvas2 = new Canvas(width, height)
-            val gc2 = canvas2.getGraphicsContext2D
-            if (t.isPlayer1)
-              gc2.setStroke(Color.BLUE)
-            else
-              gc2.setStroke(Color.RED)
-
-            val tileCenter = (x * tileSize + tileSize / 2, y * tileSize + tileSize / 2)
-
-            gc2.strokeLine(tileCenter._1, 0, tileCenter._1, height)
-            gc2.strokeLine(0, tileCenter._2, width, tileCenter._2)
-
-            boardPane.getChildren().add(canvas2)
-          }
-
-        }
-      }
-
-      drawTile(model.getTile(x, y)._1)
-      drawTile(model.getTile(x, y)._2)
+      drawCom(model.getComTile(x, y), gc2)
 
     }
 
-    (0 until Rules.terrainWidth).foreach(x => {
-      (0 until Rules.terrainHeight).foreach(y => {
-        createTileSquare(x, y)
+    def drawCom(c: (mutable.Set[Direction], mutable.Set[Direction]), gc: GraphicsContext): Unit = {
 
-      })
-    })
+      val red = c._1
+      val blue = c._2
+      gc.setLineWidth(4)
+      gc.setGlobalAlpha(0.1)
 
-    (0 until Rules.terrainWidth).foreach(x => {
-      (0 until Rules.terrainHeight).foreach(y => {
-        createTile(x, y)
-      })
-    })
+      def doDraw(dirs: mutable.Set[Direction]) = {
+        dirs.foreach(_ match {
+          case N | S => {
+            gc.moveTo(tileSize / 2, 0)
+            gc.lineTo(tileSize / 2, tileSize)
+          }
+          case NE | SW => {
+            gc.moveTo(0, tileSize)
+            gc.lineTo(tileSize, 0)
+          }
+          case E | W => {
+            gc.moveTo(0, tileSize / 2)
+            gc.lineTo(tileSize, tileSize / 2)
+          }
+          case SE | NW => {
+            gc.moveTo(0, 0)
+            gc.lineTo(tileSize, tileSize)
+          }
+        })
+      }
+
+      gc.setStroke(Color.RED)
+      doDraw(red)
+      gc.stroke
+      gc.setStroke(Color.BLUE)
+      doDraw(blue)
+      gc.stroke
+
+    }
+
+    def drawTile(t: Tile, gc: GraphicsContext): Unit = {
+      if (!t.eq(VoidTile)) {
+
+        val tileImage = new Image("tiles/" + t.char.toString + ".png")
+        gc.drawImage(tileImage, 0, 0, tileSize, tileSize)
+
+        // Draw color bar
+        if (t.isUnit) {
+          if (t.isPlayer1)
+            gc.setFill(Color.BLUE)
+          else
+            gc.setFill(Color.RED)
+          gc.fillRect(0, tileSize - tileSize / 15, tileSize, tileSize / 15)
+        }
+
+      }
+    }
+
+    // Draw Terrain
+    scanBoardCoordinates(drawTiles)
 
     boardPane
   }
 
-  def start(stage: Stage) {
-
-    model.move(14, 10, 13, 10)
-
-    val board = createBoardPane
+  def setPerspective(p: Pane): Unit = {
     val e = new PerspectiveTransform()
     e.setUlx(tileSize * 2)
     e.setUly(0)
@@ -116,13 +150,7 @@ class App extends Application {
     e.setLly(height)
     e.setLrx(width)
     e.setLry(height)
-    board.setEffect(e)
-
-    val scene = new Scene(board, Color.WHITESMOKE)
-
-    stage setTitle ("Game of War")
-    stage setScene (scene)
-    stage show
+    p.setEffect(e)
   }
 
 }
--- src/main/scala/com.lms.gow/model/Board.scala
+++ src/main/scala/com.lms.gow/model/Board.scala
@@ -2,52 +2,63 @@ package com.lms.gow.model
 
 abstract case class Tile(char: Char, speed: Int, range: Int, attack: Int,
                          defense: Int, isUnit: Boolean, isCom: Boolean, isPlayer1: Boolean)
+object VoidTile extends Tile('.', 0, 0, 0, 0, false, false, false)
+object Fortress extends Tile('F', 0, 0, 0, 4, false, false, false)
+object Mountain extends Tile('M', 0, 0, 0, 0, false, false, false)
+object MountainPass extends Tile('=', 0, 0, 0, 2, false, false, false)
+object RedArsenal extends Tile('A', 0, 0, 0, 0, true, true, false)
+object RedRelay extends Tile('R', 1, 0, 0, 1, true, true, false)
+object RedSwiftRelay extends Tile('E', 2, 0, 0, 1, true, true, false)
+object RedCannon extends Tile('C', 1, 3, 5, 8, true, false, false)
+object RedSwiftCannon extends Tile('N', 2, 3, 5, 8, true, false, false)
+object RedInfantry extends Tile('I', 1, 2, 4, 6, true, false, false)
+object RedCavalry extends Tile('V', 2, 2, 4, 5, true, false, false)
+object BlueArsenal extends Tile('a', 0, 0, 0, 0, true, true, true)
+object BlueRelay extends Tile('r', 1, 0, 0, 1, true, true, true)
+object BlueSwiftRelay extends Tile('e', 2, 0, 0, 1, true, true, true)
+object BlueCannon extends Tile('c', 1, 3, 5, 8, true, false, true)
+object BlueSwiftCannon extends Tile('n', 2, 3, 5, 8, true, false, true)
+object BlueInfantry extends Tile('i', 1, 2, 4, 6, true, false, true)
+object BlueCavalry extends Tile('v', 2, 2, 4, 5, true, false, true)
 
-object VoidTile             extends Tile('.', 0, 0, 0, 0, false, false, false)
-object Fortress             extends Tile('F', 0, 0, 0, 4, false, false, false)
-object Mountain             extends Tile('M', 0, 0, 0, 0, false, false, false)
-object MountainPass         extends Tile('=', 0, 0, 0, 2, false, false, false)
-object RedArsenal           extends Tile('A', 0, 0, 0, 0, true, true, false)
-object RedCannon            extends Tile('C', 1, 3, 5, 8, true, false, false)
-object RedSwiftCannon       extends Tile('N', 2, 3, 5, 8, true, false, false)
-object RedRelay             extends Tile('R', 1, 0, 0, 1, true, true, false)
-object RedSwiftRelay        extends Tile('E', 2, 0, 0, 1, true, true, false)
-object RedInfantry          extends Tile('I', 1, 2, 4, 6, true, false, false)
-object RedCavalry           extends Tile('V', 2, 2, 4, 5, true, false, false)
-object BlueArsenal          extends Tile('a', 0, 0, 0, 0, true, true, true)
-object BlueCannon           extends Tile('c', 1, 3, 5, 8, true, false, true)
-object BlueSwiftCannon      extends Tile('n', 2, 3, 5, 8, true, false, true)
-object BlueRelay            extends Tile('r', 1, 0, 0, 1, true, true, true)
-object BlueSwiftRelay       extends Tile('e', 2, 0, 0, 1, true, true, true)
-object BlueInfantry         extends Tile('i', 1, 2, 4, 6, true, false, true)
-object BlueCavalry          extends Tile('v', 2, 2, 4, 5, true, false, true)
+abstract case class Direction(y: Int, x: Int)
+object N extends Direction(-1, 0)
+object NE extends Direction(-1, 1)
+object E extends Direction(0, 1)
+object SE extends Direction(1, 1)
+object S extends Direction(1, 0)
+object SW extends Direction(1, -1)
+object W extends Direction(0, -1)
+object NW extends Direction(-1, -1)
 
-import Util._
+import scala.collection._
+import com.lms.gow.model.Util._
 
 object Rules {
-
-  val attacksPerTurn = 1
-  val movesPerTurn = 5
   val terrainWidth = 25
   val terrainHeight = 20
-
-  val terrainTiles = Seq(Fortress, Mountain, MountainPass)
-  val unitTiles = Seq(BlueCannon, BlueSwiftCannon, BlueRelay, BlueSwiftRelay, BlueInfantry, BlueCavalry, BlueArsenal,
+  val totalTiles = terrainWidth * terrainHeight
+  val movesPerTurn = 5
+  val attacksPerTurn = 1
+  val terrainTilesRepository = Seq(Fortress, Mountain, MountainPass)
+  val unitTilesRepository = Seq(BlueCannon, BlueSwiftCannon, BlueRelay, BlueSwiftRelay, BlueInfantry, BlueCavalry, BlueArsenal,
     RedCannon, RedSwiftCannon, RedRelay, RedSwiftRelay, RedInfantry, RedCavalry, RedArsenal)
-
-  val startingTerrain = loadTiles("src/main/resources/init.board", terrainTiles)
-  val startingUnits = loadTiles("src/main/resources/init.units", unitTiles)
+  val startingTerrain = loadTilesFromFile("src/main/resources/init.board", terrainTilesRepository)
+  val startingUnits = loadTilesFromFile("src/main/resources/init.units", unitTilesRepository)
+  val directions = Seq(N, NE, E, SE, S, SW, W, NW)
 }
 
 object Util {
 
   def indexFromCoordinates(x: Int, y: Int) = (x + (y * Rules.terrainWidth))
 
-  def loadTiles(file: String, tileRef: Seq[Tile]): Seq[Tile] = {
-    scala.io.Source.fromFile(file)
+  //def coordinatesFromIndex(x: Int): (Int, Int) = (x % Rules.terrainWidth, x / Rules.terrainWidth)
+
+  def loadTilesFromFile(file: String, tileRepository: Seq[Tile]): Seq[Tile] = {
+    io.Source.fromFile(file)
       .mkString
       .filter(_ > ' ')
-      .map((tileRef.map(_.char) zip tileRef)
+      .map((tileRepository.map(_.char) zip tileRepository)
         .toMap
         .get(_)
         .getOrElse(VoidTile))
@@ -57,42 +68,94 @@ object Util {
 
 class Board {
 
+  val terrainLayer = Rules.startingTerrain
+  var unitLayer = Rules.startingUnits.zipWithIndex.map(_.swap).toMap
+  val comLayerRed : Seq[mutable.Set[Direction]] = Seq.fill(Rules.totalTiles)(mutable.Set[Direction]())
+  val comLayerBlue : Seq[mutable.Set[Direction]] = Seq.fill(Rules.totalTiles)(mutable.Set[Direction]())
+
   var player1Turn = true
   var leftMoves = Rules.movesPerTurn
-  var currentUnits = Rules.startingUnits.zipWithIndex.map(_.swap).toMap
+
+  refreshComLayer
+
+  def refreshComLayer = {
+
+    comLayerRed.foreach(d => d.clear)
+    comLayerBlue.foreach(d => d.clear)
+
+    unitLayer.filter(u => u._2.eq(RedArsenal) || u._2.eq(BlueArsenal))
+      .map(propagateCom(_))
+
+    def propagateCom(source: (Int, Tile)): Unit = {
+
+      Rules.directions.foreach(dir => {
+
+        var doRun = true
+        var index = source._1
+
+        while (doRun) {
+          index += dir.x + (dir.y * Rules.terrainWidth)
+          if (index % Rules.terrainWidth < 1 || !(0 until Rules.totalTiles).contains(index)
+            || !shouldPropagateCom(source._2.isPlayer1, terrainLayer(index), unitLayer(index))) {
+            doRun = false
+          } else {
+            if (source._2.isPlayer1) {
+              comLayerBlue(index) += dir
+            } else {
+              comLayerRed(index) += dir
+            }
+          }
+        }
+      })
+
+      def shouldPropagateCom(player1: Boolean, terrain: Tile, unit: Tile): Boolean = {
+        if (terrain == Mountain ||
+          (Rules.unitTilesRepository.contains(unit)
+            && unit.isPlayer1 != player1))
+          false
+        else
+          true
+      }
+
+    }
+
+  }
+
+  def getUnitTile(x: Int, y: Int) = unitLayer(indexFromCoordinates(x, y))
+  def getTerrainTile(x: Int, y: Int) = terrainLayer(indexFromCoordinates(x, y))
+  def getComTile(x: Int, y: Int) = (comLayerRed(indexFromCoordinates(x, y)), comLayerBlue(indexFromCoordinates(x, y)))
 
   def move(x: Int, y: Int, newX: Int, newY: Int): Boolean = {
 
     val srcCoord = indexFromCoordinates(x, y)
     val dstCoord = indexFromCoordinates(newX, newY)
-    val unit = currentUnits.get(srcCoord).getOrElse(VoidTile)
-    val dstUnit = currentUnits.get(dstCoord).getOrElse(VoidTile)
+    val unit = unitLayer.get(srcCoord).getOrElse(VoidTile)
+    val dstUnit = unitLayer.get(dstCoord).getOrElse(VoidTile)
     val dstTerrain = Rules.startingTerrain(dstCoord)
 
     if (unit.isPlayer1 == player1Turn
       && dstUnit.equals(VoidTile)
       && !dstTerrain.equals(Mountain)
       && leftMoves > 0) {
-        leftMoves = leftMoves - 1
-        if (leftMoves == 0) {
-          leftMoves = Rules.movesPerTurn
-          player1Turn = !player1Turn
-        }
-        currentUnits += srcCoord -> VoidTile
-        currentUnits += dstCoord -> unit
-        return true
+      leftMoves = leftMoves - 1
+      if (leftMoves == 0) {
+        leftMoves = Rules.movesPerTurn
+        player1Turn = !player1Turn
+      }
+      unitLayer += srcCoord -> VoidTile
+      unitLayer += dstCoord -> unit
+
+      refreshComLayer
+
+      return true
     } else {
       return false
     }
   }
 
-  def getTile(x: Int, y: Int): (Tile, Tile) = {
-    val coord = indexFromCoordinates(x, y)
-    (Rules.startingTerrain(coord), currentUnits(coord))
-  }
 
   def dump {
-    for (tile <- currentUnits) {
+    for (tile <- unitLayer) {
       val t = tile._2
       print(s"$t  ")
       if ((tile._1 + 1) % Rules.terrainWidth == 0)
